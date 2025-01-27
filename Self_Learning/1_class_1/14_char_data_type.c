c9b30a5724936033a4021cd48503353af73964b1
sanu-programmer <sanuk8137@gmail.com>
2025-01-27 22:25:27 +0530

    user input for char data type

--- Self_Learning/1_class_1/14_char_data_type.c
+++ Self_Learning/1_class_1/14_char_data_type.c
@@ -4,5 +4,19 @@ int main(){
     printf("%c\n",ch);
     char ch1 ='A';
     printf("%d",ch1);
+    #include <stdio.h>
+
+
+    char character;
+
+    // Taking character input from user
+    printf("Enter a character: ");
+    scanf(" %c", &character);  // Notice the space before %c to consume any leading whitespace
+
+    // Printing the character
+    printf("You entered: %c\n", character);
+
     return 0;
 }
+
+
